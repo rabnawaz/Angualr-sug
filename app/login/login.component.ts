298273e4a2092cd6e926674b526c48ec8684cd19
Rab Nawaz <rabnawazlhr@gmail.com>
2018-07-24 20:18:28 +0500

    Add files via upload

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -12,11 +12,14 @@ import { AuthService } from '../shared/auth.service';
 
 
 export class LoginComponent implements OnInit{
-    
+    items: any;
     constructor(private Auth: AuthService){}
 
     ngOnInit(){
-
+        const self = this;
+        this.Auth.getUserDetails('','', function(data) {
+            self.items = data;
+        });
     }
 
     loginUser(event){
@@ -25,7 +28,7 @@ export class LoginComponent implements OnInit{
         const username = target.querySelector('#username').value
         const password = target.querySelector('#password').value
 
-        this.Auth.getUserDetails(username , password)
+        this.Auth.getUserDetails(username , password, null)
         
         //console.log("username and password" , username , password)
         //console.log($event.target.value);
--- app/shared/auth.service.ts
+++ app/shared/auth.service.ts
@@ -1,23 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 
+interface recordsData {
+    records: Array<any>;
+}
+
 @Injectable()
-export class AuthService {
-    myData : any;
+export class AuthService implements OnInit {
+    items : Array<any>;
+    data: recordsData;
     constructor(private http: Http){
 
     }
+
+    ngOnInit() {
+       //this.getUserDetails('', ''); 
+    }
     
-    getUserDetails(username, password){
+    getUserDetails(username, password, callback){
         this.http.get('http://localhost/SugarApi.php', {})
         .subscribe(
-        (data) => {
-            data = JSON.parse(data['_body']);
+        (response) => {
+            
+            this.data = JSON.parse(response['_body']);
+            this.items = this.data.records;
+            callback(this.items);
             //console.log(data); 
-            this.myData = data;
+            //this.items = data.records;
 
-           console.log("My data", data);
+           //console.log("My data", data);
         },
         err => console.log(err), // error
         () => console.log('getUserStatus Complete') // complete
